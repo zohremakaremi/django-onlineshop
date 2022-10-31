710399f89ebcdefb287fffd5a37d6f9d3097fcad
zohre <zohremakaremi1997@gmail.com>
2022-10-31 11:30:52 +0330

    add slider-settings-contact app

--- eshop_products/urls.py
+++ eshop_products/urls.py
@@ -5,7 +5,7 @@ app_name = 'eshop_products'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-	path('list/', views.ListView.as_view(), name='product_list'),
+	path('', views.ListView.as_view(), name='product_list'),
 	path('category/<slug:category_slug>/', views.ListView.as_view(), name='category_filter'),
 	path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
